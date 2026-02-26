65114893fcb00d4373b86e9ac93a4882a7c5f19c
karine-tiecher <tiecherkarine@gmail.com>
2026-02-26 12:15:27 -0300

    Inicio com JS

---
+++ inicio/comentarios.js
@@ -0,0 +1,24 @@
+// O console é utilizado para imprimir algo na tela 
+
+console.log("Sesi");
+//console.log("Senai");
+
+/*
+
+console.log("Senai");
+console.log("Senai");
+console.log("Senai");
+console.log("Senai");
+
+*/
+
+
+
+
+
+
+
+
+
+
+
---
+++ inicio/desafio_bloco.js
@@ -0,0 +1,13 @@
+/* 
+Desafio: Criar dois blocos de código.
+Onde o primeiro deve imprimir na tela os 
+números pares de 0 a 10 e o segundo 
+bloco deve imprimir na tela os ímpares
+*/
+
+{
+console.log(" numeros pares: 0, 2, 4, 6, 8, 10, ");
+}
+{
+console.log("numeros impares: 1, 3, 5, 7, 9,  ");
+}
