2b1f7b13f42b0e496379a957ee81588023c1d005
karine-tiecher <tiecherkarine@gmail.com>
2026-02-27 11:53:20 -0300

    Tipos de dados

---
+++ inicio/dados2.js
@@ -0,0 +1,17 @@
+//console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+//console.log(precoComDesconto);
+
+let nome = "Caderno"; 
+let categoria = "Papel";
+console.log("Produto: " + nome + 
+    ", Categoria: " + categoria + 
+    ", Preco " + preco +
+    " Desconto " + desconto);
+    
+
+
+
