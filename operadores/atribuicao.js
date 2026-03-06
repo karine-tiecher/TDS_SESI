0a0110a75f14cd6f21d0b4f8999d277382181d9d
karine-tiecher <tiecherkarine@gmail.com>
2026-03-06 11:39:20 -0300

    Operações de atribução e relacionais

---
+++ operadores/atribuicao.js
@@ -0,0 +1,19 @@
+let x = 100;
+x = x + 5;
+x += 5;
+x -= 5;
+x *= 5; //525
+x /= 5; //105
+x %= 5; //0
+console.log(x);
+
+let y = 200;
+y = y + 100;
+y += 100;
+console.log(y);
+
+let nome = "Karine ";
+nome += "Tiecher";
+
+console.log(nome);
+
---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 11;
+ 
+// imprimir no console 5 números verdadeiros(true)
+console.log(a <= b);
+console.log(a == a);
+console.log(a != b );
+console.log( b !== a);
+console.log(b >= a);
+
+// imprimir no console 5 números falsos(false)
+console.log(a === b);
+console.log(a >= b);
+console.log(a >= b);
+console.log(b <= a);
+console.log(b === a);
+
+
