1bc2f95eb82627283e96d4fa0a05a5dc6645b5a6
karine-tiecher <tiecherkarine@gmail.com>
2026-03-13 12:17:17 -0300

    Array js

---
+++ array/aray2.js
@@ -0,0 +1,10 @@
+const nomes = [];
+// Desafio: Inserir 5 novos indices com 5 nomes e imprimir na tela 
+// indices         0         1      2         3         4
+const amigas = ["Karine", "Ana", "Sther", "Monica", "Nicoly"];
+console.log(amigas[0]);
+console.log(amigas[1]);
+console.log(amigas[2]);
+console.log(amigas[3]);
+console.log(amigas[4]);
+console.log(amigas);
---
+++ array/desafio_array.js
@@ -0,0 +1,27 @@
+const numeros = [];
+
+// Inserir 10 numeros utilizando a função .push()
+// e imprimir na tela utilizando o formato: 'Numero x"
+
+const numero = [10, 20, 30,]
+console.log(typeof numeros);
+numeros.push(1);
+numeros.push(2);
+numeros.push(3);
+numeros.push(4);
+numeros.push(5);
+numeros.push(6);
+numeros.push(7);
+numeros.push(8);
+numeros.push(9);
+numeros.push(10);
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
+console.log("Numero" + numeros[0];
