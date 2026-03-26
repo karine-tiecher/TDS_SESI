b28b7720551e6ac22f20710be0ebf17fa9bb1d49
karine-tiecher <tiecherkarine@gmail.com>
2026-03-26 11:37:59 -0300

    Exemplso.js

---
+++ atividades/atividade1.js
@@ -0,0 +1,11 @@
+// Declare variáveis para armazenar o nome de um produto (string)
+// o preço unitário (Number) e se ele está em estoque (boolean)
+
+let produto = "Teclado";
+let preco = 500;
+let estoque = true;
+
+console.log(O item é  + produto +  custa R$${preco});
+console.log()
+
+
